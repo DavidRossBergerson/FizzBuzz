7315117861c64373ad8b19fadb9aee30d3ddb59a
DavidRossBergerson <73494751+DavidRossBergerson@users.noreply.github.com>
2021-01-12 16:51:17 -0500

    more touchups

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,8 +1,10 @@
 
-// This uses an anonymous function to call a named function
-document.getElementById("fizzButton").addEventListener("click", function(){
+    // This uses an anonymous function to call a named function
+    document.getElementById("fizzButton").addEventListener("click", function(){
+    
     let num1 = 1
     let num2 = 100
+    
     if(num1 > num2){
         alert("Number 1 must be less than Number 2");
     }else{
@@ -13,19 +15,25 @@ document.getElementById("fizzButton").addEventListener("click", function(){
     
 });
 
-// This is the named function, it will only run when something else calls it
-function runTheNumbers(num1, num2){
+    // This is the named function, it will only run when something else calls it
+    function runTheNumbers(num1, num2){
     for (let loop = num1; loop <= num2; loop++) {
     
-    if(loop % 15 === 0){
-        document.getElementById("results").innerText += `FizzBuzz-`
+
+
+    
+    if(loop % 15 === 0){ 
+        document.getElementById("results").innerHTML += `<span class="fizz">FizzBuzz-</span>`
+
    
     }else if (loop % 3 === 0){
-        document.getElementById("results").innerText += `Fizz-`
+        document.getElementById("results").innerHTML +=  `<span class="fizz">Fizz-</span>`
+
     }else if (loop % 5 === 0){
-        document.getElementById("results").innerText += `Buzz-`
+        document.getElementById("results").innerHTML +=  `<span class="fizz">Buzz-</span>`
+
     }else{
-         document.getElementById("results").innerText += `${loop}-`
+         document.getElementById("results").innerHTML += `${loop}-`
     }
     
     }
